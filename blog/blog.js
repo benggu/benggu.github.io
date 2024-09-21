540f6d86e3e4cf5ecd460605a2b60064450056a8
benggu <pointyhat430@gmail.com>
2024-09-21 15:22:14 +0900

    Add files via upload

---
+++ blog/blog.js
@@ -0,0 +1,8 @@
+cartoon_blogs=[]
+games_blogs=[]
+diary_entries=[]
+photo_logs=[]
+
+function loadBlogList(list){
+
+}
---
+++ navbar.js
@@ -0,0 +1,7 @@
+document.querySelector("header").innerHTML=`<a href="/"><h1>microbenggu place</h1></a> <img src="/logo.png" style="height:100%"> -- <span id="tagline" style="font-style: italic;">"it sure is great here"</span>
+            <form id="searchbar">
+                <input type="text" placeholder="totally necessary search bar">
+                <input type="submit" value="Go!">
+            </form>`
+document.getElementById("navbar").innerHTML=`<a href="/">home</a> <a href="#">writings</a> <a href="#">fun and games</a> <a href="#">smoking room</a>`
+document.querySelector("footer").innerHTML=`© microbenggu 2024 - hosted on github pages<br>contact me housepenbenggu@gmail.com`
