a711c9f9c6488673c02cc9a979f75641598b2283
benggu <pointyhat430@gmail.com>
2024-09-21 15:47:45 +0900

    added blog index page makers

--- blog/blog.js
+++ blog/blog.js
@@ -1,8 +1,16 @@
-cartoon_blogs=[]
-games_blogs=[]
-diary_entries=[]
-photo_logs=[]
+shows_blogs=[{url:"/blog/shows/exampleblog.html",title:"title",desc:"an example blog."}];
+games_blogs=[];
+diary_entries=[];
+photo_logs=[];
 
 function loadBlogList(list){
-
+    var bloglist=document.getElementById("bloglist");
+    bloglist.innerHTML="";
+    for(const i in shows_blogs){
+        var k=list[i];
+        var x=document.createElement("li");
+        x.innerHTML=`<a href="${k.url}"><h2>${k.title}</h2></a>
+                     <p>${k.desc}</p>`;
+        bloglist.appendChild(x);
+    }
 }
